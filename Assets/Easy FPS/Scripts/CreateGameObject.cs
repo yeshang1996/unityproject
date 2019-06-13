6524aef68fe173d3b0de11a27fa0cd5c22227562
yeshang1996 <979572640@qq.com>
2019-06-13 21:32:06 +0800

    untiy
    unity课程大作业

---
+++ Assets/Easy FPS/Scripts/CreateGameObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateGameObject : MonoBehaviour
+{
+    //初始生成时间5秒钟
+
+    float times = 5f;
+
+    //物体
+    public GameObject wolf1;
+    GameObject targert = null;
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+
+        times -= Time.deltaTime; //减时间
+        if (times < 0) //倒计时
+        {
+            //产生物体
+            GameObject obj = (GameObject) Instantiate(wolf1);
+            int ni = Random.Range(-9, 18);
+            int nt = Random.Range(25, 16);
+
+            //随机位置
+            obj.transform.position = new Vector3(ni, 5.8f, nt);
+
+
+            //重新设置时间为0-10之间的一个随机数   随机时间
+            times = Random.Range(0, 10);
+
+
+        }
+    }
+}
---
+++ Assets/Easy FPS/Scripts/DUMMINE.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DUMMINE : MonoBehaviour
+{
+    
+    public GameObject DM; 
+    
+    
+    // Start is called before the first frame update
+    
+        
+    void Start()
+    {  
+        DM = GameObject.FindWithTag("Dummie");
+         
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.FindWithTag("blood") != null)
+        {  
+           int x=Random.Range(-15,15);
+           int z=Random.Range(-25,15);
+           int y=Random.Range(5,15);
+           Vector3 position=new Vector3(x,y,z);
+           GameObject newGo=Instantiate(DM,position,transform.rotation);
+            Destroy(this.gameObject);
+        }
+    }
+
+}
